90c5594c39ef575cec74aeeaed35cbcda28468c0
rapirawr <animekayangg313@gmail.com>
2024-09-06 19:32:28 +0700

    Update

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
 const photoGallery = document.getElementById('photo-gallery');
 
-const images = [];
-for (let i = 1; i <= 250; i++) {
-    images.push(`Foto/Image${i}.jpg`);
+// Cek apakah pengguna sudah login
+function isLoggedIn() {
+    const storedUsername = localStorage.getItem('username');
+    return storedUsername !== null; // Mengembalikan true jika username ada di localStorage
 }
 
-images.forEach((src, index) => {
-    const a = document.createElement('a');
-    a.href = src;
-    a.download = `Image${index + 1}`;
+if (isLoggedIn()) {
+    const images = [];
+    for (let i = 1; i <= 250; i++) {
+        images.push(`Foto/Image${i}.jpg`);
+    }
+
+    images.forEach((src, index) => {
+        const a = document.createElement('a');
+        a.href = src;
+        a.download = `Image${index + 1}`;
+        
+        const img = document.createElement('img');
+        img.src = src;
+        img.alt = `Image ${index + 1}`;
+        
+        // Event listener untuk gambar yang gagal dimuat
+        img.onerror = function() {
+            img.style.visibility = 'hidden'; // Menyembunyikan gambar jika gagal dimuat
+        };
+        
+        a.appendChild(img);
+        photoGallery.appendChild(a);
+    });
+} else {
+    // Jika belum login, tampilkan pesan dengan ikon login
+    const messageDiv = document.createElement('div');
+    messageDiv.classList.add('gallery-message');
     
-    const img = document.createElement('img');
-    img.src = src;
-    img.alt = `Image ${index + 1}`;
+    const textParagraph = document.createElement('p');
+    textParagraph.textContent = 'Silahkan login untuk melihat profil';
     
-    // Event listener untuk gambar yang gagal dimuat
-    img.onerror = function() {
-        img.style.visibility = 'hidden'; // Menyembunyikan gambar jika gagal dimuat
-    };
+    const link = document.createElement('a');
+    link.href = 'login.html';
     
-    a.appendChild(img);
-    photoGallery.appendChild(a);
-});
+    const icon = document.createElement('i');
+    icon.className = 'fa-solid fa-right-to-bracket';
+    icon.style.color = ' #00fbff';
+    
+    // Menambahkan gaya inline untuk memastikan elemen berada di sebelah satu sama lain
+    messageDiv.style.display = 'flex';
+    messageDiv.style.alignItems = 'center';
+    
+    textParagraph.style.margin = '0';
+    textParagraph.style.fontSize = '1em'; // Sesuaikan ukuran font jika perlu
+    
+    link.style.marginLeft = '8px'; // Jarak antara teks dan ikon
+    
+    icon.style.fontSize = '1.2em'; // Sesuaikan ukuran ikon jika perlu
+    
+    link.appendChild(icon);
+    messageDiv.appendChild(textParagraph);
+    messageDiv.appendChild(link);
+    photoGallery.appendChild(messageDiv);
+}
 
 // text animasi
 document.addEventListener('DOMContentLoaded', function() {
